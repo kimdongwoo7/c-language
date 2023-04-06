bbef9eeea3456560c4639312da110e2e7ec43b30
김동우 <dj03017@gmail.com>
2023-04-06 05:21:19 +0000

    나의 첫 프로그래밍 47b.c

---
+++ 47b.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	int num1=16;
+	int num2;
+	int sum=0; int sub; int div; int nu1;
+	num2 = 61;
+	sum = num1 + num2;
+	printf("%d - %d = %d\n",num1 ,num2 ,sum);
+	printf("%d * %d = %d\n",num1 ,num2 ,nu1);
+    return 0;
+}
