d873749907c511f1bb0f36ecd3844fb3a3385831
김동우 <dj03017@gmail.com>
2023-04-20 06:01:05 +0000

    04.20 5교시

--- calc/59.c
+++ calc/59.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 int main() 
 {
-	printf("%d %d\n", 3&1, 3|1);
-	printf("%d\n", 3^1);
-	printf("%d %d\n", 3<<1, 3>>1);
-	printf("%d\n", ~3);
-	
+	int a = 5, b = 7, c, d, e, f;
+	c= a & b;
+	d = a | b;
+	e = a ^ b;
+	f = ~b;
+	a = a >> 1;
+	b = b << 3;
+	printf("%d, %d, %d\n", a, b, c);
+	printf("%d, %d, %d\n", d, e, f);
 	return 0;
 }
-	
